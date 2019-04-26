0a305964bbdebbc6dcb2fcf7a4aa29ed18f73fd3
ZHANG Zhen <zen@ZHANGs-MacBook-Pro.local>
2019-04-25 20:54:32 -0400

    final version

--- final/f.m
+++
@@ -1,3 +0,0 @@
-function y=f(x)
-    y=pi^2*sin(pi*x);
-end
--- final/main.m
+++ final/main.m
@@ -70,6 +70,21 @@ for i=1:numtrial
         uu=uu/h^2;
         l2=l2+(uexact(testx(p))-uu)^2;
     end
+    uuu=zeros(M+2,1);
+    for p=1:M+2
+        j=p;
+        if (j==M+2) 
+            j=j-1; 
+        end
+        if (j==1)
+            uuu(p)=u(1)*basis1(x(p),0,h)+u(2)*basis1(x(p),h,h)+u(3)*basis2(x(p),h,h);
+        elseif (j==M+1)
+            uuu(p)=u(2*M)*basis1(x(p),M*h,h)+u(2*M+1)*basis2(x(p),M*h,h)+u(2*M+2)*basis1(x(p),(M+1)*h,h);
+        else
+            uuu(p)=u(2*j-2)*basis1(x(p),(j-1)*h,h)+u(2*j-1)*basis2(x(p),(j-1)*h,h)+u(2*j)*basis1(x(p),j*h,h)+u(2*j+1)*basis2(x(p),j*h,h);
+        end
+        uuu(p)=uuu(p)/h^2;
+    end
     l2=sqrt(l2/numeval);
     bderror1(i)=abs(-4/h*u(1)-2/h*u(2)+6/h^2*u(3));
     bderror2(i)=abs(u(2*M)*2/h+u(2*M+1)*6/h^2+u(2*M+2)*4/h+2*u(2*M+2));
@@ -78,12 +93,12 @@ for i=1:numtrial
 end
 
 %% Plot graph and summarize errors
-plot(x,exactu)
-legend('Exact Solution')
+plot(x,exactu,x,uuu)
+legend('Exact Solution','Numerical Solution')
 savefig('result/solution.fig')
 for i=2:numtrial
     rate(i-1)=log(error(i-1)/error(i))/log(2); %rate for l2 error
     rate2(i-1)=log(bderror1(i-1)/bderror1(i))/log(2); %rate for left boundary error
     rate3(i-1)=log(bderror2(i-1)/bderror2(i))/log(2); %rate for right boundary error
 end
-save('data/error_rate.fig','error','bderror1','rate')
+save('data/error_rate.fig','error','bderror1','bderror2','rate','rate2','rate3')
